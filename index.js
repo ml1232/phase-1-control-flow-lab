29069184e92ac984e9c31b79cbc56feb012b65b3
Matthew <matthewjaredlevin88@gmail.com>
2023-06-13 19:52:32 -0500

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result = ('This one is on me!', 'I will gladly take your 30 bucks.', 'No can do.');
+  if (someNumber <= 400) {
+    result= 'This one is on me!';
+  }
+    else if (someNumber > 400 && someNumber < 2000) {
+      result = 'That will be twenty bucks.';
+  }
+      else if (someNumber > 2000 && someNumber < 2500) {
+        result = 'I will gladly take your thirty bucks.';
+    }
+        else if (someNumber > 2500) {
+         result = 'No can do.';
+      }
+      return result
+
+}
+function ternaryCheckCity(destination, tip) {
+  return destination !== "NYC" ? "No go." :
+    "Ok, sounds good." 
+    tip > 20 ? "Thank you so much." :
+    "Thank you."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.';
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
